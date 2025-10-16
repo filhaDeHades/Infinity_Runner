91355e3d64c5de07174179857636c973c66fabb8
Tamires da Hora <thora@id.uff.br>
2025-10-16 08:59:43 -0300

    Add efeito parallax, camera e movimento lateral do player

---
+++ Personagem/Scripts/movimentoPersonagem.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+const VELOCIDADE = 150
+var movimento = Vector2.ZERO
+var gravidade = 100
+var esquerda = true
+
+func _physics_process(delta):
+	var direcao := Vector2(Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * VELOCIDADE)
+	
+	movimento.x = direcao.x
+	#movimento.y += gravidade * delta
+	
+	if direcao.y < 0:
+		pass
+	
+	
+#	if movimento != Vector2.ZERO:
+#		esquerda = movimento[0] < 0
+#		$AnimatedSprite.flip_h = esquerda
+#
+#		$AnimatedSprite.play("Run")
+#	else:
+#		$AnimatedSprite.play("idle")
+	
+	move_and_slide(movimento)
